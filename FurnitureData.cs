fe2186481be0288911342eda4831f62f3eecfdea
NiniPomelo <144758496+NiniPomelo@users.noreply.github.com>
2026-07-09 10:42:23 +0800

    Delete FurnitureData.cs

--- FurnitureData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class FurnitureData
-{
-    public string name;
-    public Vector3 relativePosition; // 相對房間中心
-    public Vector3 size;             // 家具尺寸 (x=寬, y=高, z=深)
-}
